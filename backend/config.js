adfa73fde018041f65a8054497aa168d24d581ba
Rao <justin.rao@ec.gc.ca>
2025-09-09 08:37:11 -0700

    Reset the Station folder location

--- backend/config.js
+++ backend/config.js
@@ -2,8 +2,8 @@
 // UNC base folder where station photo folders live
 module.exports = {
   // Also used as the base station folder for inspection history and construction history
-  //PHOTOS_BASE: '\\\\Ecbcv6cwvfsp001.ncr.int.ec.gc.ca\\msc$\\401\\WSCConstruction\\Stations',
-  PHOTOS_BASE: 'C:\\Users\\nitsu\\OneDrive\\Documents\\Stations',
+  PHOTOS_BASE: '\\\\Ecbcv6cwvfsp001.ncr.int.ec.gc.ca\\msc$\\401\\WSCConstruction\\Stations',
+  //PHOTOS_BASE: 'C:\\Users\\nitsu\\OneDrive\\Documents\\Stations',
 
   IMAGE_EXTS: ['.jpg', '.jpeg', '.png', '.gif', '.webp', '.bmp', '.tif', '.tiff'],
 };
